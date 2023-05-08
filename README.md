ddd73a8e1c1838f634c883f3ef28ccae9fd5b00e
DDD10Decade <102458086+DDD10Decade@users.noreply.github.com>
2023-05-08 23:18:33 +0800

    初始

---
+++ README.md
@@ -0,0 +1,25 @@
+# frontend
+##词云网站前端
+## Project setup
+##配置文件
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+    </div>
+    <button @click="test()">CheckThis</button>
+    <div>{{ text }}</div>
+    <div></div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'HelloWorld',
+    props: {
+        msg: String
+    },
+    data() {
+        return {
+            text: 'null'
+        }
+    },
+    methods: {
+        test() {
+            axios.get('test')
+                .then(res => {
+                    this.text = res.data;
+                })
+                .catch(err => {
+                    this.text = 'error' + err;
+                })
+            // this.text = 'nice!'
+        },
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+    margin: 40px 0 0;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+div {
+    display: inline-block;
+    margin: 0 10px;
+}
+
+a {
+    color: #42b983;
+}
+</style>
